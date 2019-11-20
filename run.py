6ab7a374f6f889d93db98089a9d2830814c2f7f7
Michael <michael.morris.london@gmail.com>
2019-11-20 16:15:33 +0000

    added save

--- run.py
+++ run.py
@@ -93,9 +93,10 @@ network.train(train_x=Lx,
               train_y=Ly,
               unlabelled_x=Ux,
               val_x = x_test, val_y=y_test,
-              epochs=20,
+              epochs=1,
               Lambda = 0.5,
               labelled_batch_size=32,
               unlabelled_batch_size = 256)
 
 
+network.model.save('UDA_Model.hdf5')
