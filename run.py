c28579a8e4cd6f1b95afe53d7c04fd08bba5d32c
Michael <michael.morris.london@gmail.com>
2019-11-21 14:25:25 +0000

    changed to cifar10

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ import sys
 import os
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten, LeakyReLU
+from tensorflow.keras.layers import Activation, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, LeakyReLU
 from tensorflow.keras.layers import BatchNormalization
 import parser
 
@@ -26,59 +26,48 @@ datagen = ImageDataGenerator(
 #sys.stdout = open('output.txt','wt')
 
 # Fetch and format the mnist data
-(x_train, y_train), (x_test, y_test), = tf.keras.datasets.mnist.load_data()
-x_train=x_train/255
-x_test=x_test/255
-datagen.fit(x_test.reshape((x_test.shape[0], 28, 28, 1)))
 
-datset_size = np.size(y_train)
-split = 0.05
-split = int(np.floor(datset_size*split))
+(x_train, y_train), (x_test, y_test), = tf.keras.datasets.cifar10.load_data()
+num_classes=10
 split = 200
+
+y_train = np.squeeze(y_train)
+y_test = np.squeeze(y_test)
+x_train=x_train/np.max(x_train)
+x_test=x_test/np.max(x_test)
+datagen.fit(x_train)
+
 Lx = x_train[:split]
 Ly = y_train[:split]
 
 Ux = x_train[split:]
 
-Ux = Ux[:,:,:,np.newaxis]
-Lx = Lx[:,:,:,np.newaxis]
-x_test = x_test[:,:,:,np.newaxis]
+# Ux = Ux[:,:,:,np.newaxis]
+# Lx = Lx[:,:,:,np.newaxis]
+# x_test = x_test[:,:,:,np.newaxis]
 
-
-
-
-weight_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 model = Sequential()
-model.add(Conv2D(64, kernel_size=4, input_shape=(28, 28, 1), padding="same", strides=2, kernel_initializer=weight_init))
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-model.add(BatchNormalization())
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=2, strides=1))
-model.add(Dropout(0.5))
-
-model.add(Conv2D(128, kernel_size=4, padding="same", strides=2, kernel_initializer=weight_init))
-model.add(BatchNormalization())
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=2, strides=1))
-model.add(Dropout(0.5))
-
-model.add(Conv2D(256, kernel_size=4, padding="same", strides=2, kernel_initializer=weight_init))
-model.add(BatchNormalization())
-model.add(LeakyReLU())
-model.add(MaxPooling2D(pool_size=2, strides=1))
-model.add(Dropout(0.5))
-
-model.add(Conv2D(256, kernel_size=4, padding="same", strides=2, kernel_initializer=weight_init))
-model.add(BatchNormalization())
-model.add(LeakyReLU())
-model.add(Dropout(0.5))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(1024, kernel_initializer=weight_init))
-model.add(BatchNormalization())
-model.add(LeakyReLU())
+model.add(Dense(512))
+model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
 
 
 
