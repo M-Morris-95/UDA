2197b209ce2c2954dc1036a8823f0ff42d2b9170
Michael <michael.morris.london@gmail.com>
2019-11-27 15:52:34 +0000

    added logging

--- SimpleAugment.py
+++ SimpleAugment.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
-import numpy as np
+
 from simple_network import Simple_Consistency_Regularisation
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, LeakyReLU
 from DataGenerator import datagen, Get_Data
-
-
+import os
+import time
+import sys
 
 
 (xl_train, yl_train, xu_train), (x_test, y_test), (x_val, y_val) = Get_Data('CIFAR10', 0.1, 4000)
@@ -35,5 +35,20 @@ model.add(Dropout(0.5))
 model.add(Dense(10))
 
 
-network = Simple_Consistency_Regularisation(model, datagen=datagen, KL_D=False)
+network = Simple_Consistency_Regularisation(model, epochs=1, datagen=datagen, KL_D=False)
 network.train(model, xl_train, yl_train, xu_train, x_val, y_val)
+
+
+t = time.localtime()
+timestamp = time.strftime('%b-%d-%Y_%H%M', t)
+
+os.chdir('logging')
+os.mkdir(timestamp)
+os.chdir(timestamp)
+
+text_file = open("parameters.txt", "w")
+text_file.write(' '.join(sys.argv[1:]))
+text_file.close()
+
+network.history.to_csv (r'history.csv', index = None, header=True)
+network.model.save('UDA_Model.hdf5')
